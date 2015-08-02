b00b1b06aae5ed20e19ecd9319039bfd2ad24ec1
lipikadugar <lipikad@thoughtworks.com>
2015-08-02 14:46:17 +0530

    [Lipika] |#0047| Added test specs to check the equality of two movie names.

--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -4,6 +4,7 @@ import com.twu.biblioteca.model.Movie;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class MovieTest {
 
@@ -13,5 +14,21 @@ public class MovieTest {
         Movie movie2 = new Movie("Men In Black", "Barry Sonnenfeld", 1997, 7, true);
 
         assertEquals(true, movie1.equals(movie2.getName()));
+    }
+
+    @Test
+    public void shouldCompareTheEqualityOfTwoMoviesBasedOnTheirNames() {
+        Movie movie1 = new Movie("Men In Black", "Barry Sonnenfeld", 1997, 7, true);
+        Movie movie2 = new Movie("Bajrangi BhaiJaan", "Kabir Khan", 2015, 8, true);
+
+        assertNotEquals(true, movie1.equals(movie2));
+    }
+
+    @Test
+    public void shouldGiveTheEqualityOfMovieNameAndTheMovieClass() {
+        Movie movie1 = new Movie("Men In Black", "Barry Sonnenfeld", 1997, 7, true);
+        String movie2 = "Men In Black";
+
+        assertEquals(true, movie1.equals(movie2));
     }
 }
