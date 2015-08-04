b6c670b30a76c838799050824b117ec32df3b978
lipikadugar <lipikad@thoughtworks.com>
2015-08-04 18:15:37 +0530

    [Lipika] |#0095| Displays an error message if user is not authenticated.

--- src/com/twu/biblioteca/operation/PromptUser.java
+++ src/com/twu/biblioteca/operation/PromptUser.java
@@ -2,18 +2,19 @@ package com.twu.biblioteca.operation;
 
 import com.twu.biblioteca.model.Library;
 import com.twu.biblioteca.model.UserAuthentication;
+import com.twu.biblioteca.view.LibrarianSession;
 import com.twu.biblioteca.view.View;
 
 import static com.twu.biblioteca.view.Messages.*;
 
 public class PromptUser implements Operations {
     private final View view;
-    private final UserAuthentication librarian;
+    private final UserAuthentication user;
     private final Library library;
 
     public PromptUser(View view, UserAuthentication librarian, Library library) {
         this.view = view;
-        this.librarian = librarian;
+        this.user = librarian;
         this.library = library;
     }
 
@@ -23,5 +24,10 @@ public class PromptUser implements Operations {
         String userID = view.input();
         view.print(PASSWORD);
         String password = view.input();
+        if (user.authenticate(userID, password)) {
+            new LibrarianSession(view, library).execute();
+        }
+        else
+            view.print(INVALID_USER);
     }
 }
--- test/com/twu/biblioteca/PromptUserTest.java
+++ test/com/twu/biblioteca/PromptUserTest.java
@@ -7,8 +7,7 @@ import com.twu.biblioteca.view.View;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class PromptUserTest {
 
@@ -24,5 +23,21 @@ public class PromptUserTest {
         verify(view).print("Enter user ID : ");
         verify(view).print("Enter Password: ");
         verify(view, times(2)).input();
+    }
+
+    @Test
+    public void shouldBeAbleToDisplayInvalidUserMessageIfUserIsFailedToAuthenticated() {
+        View view = Mockito.mock(View.class);
+        UserAuthentication user = Mockito.mock(UserAuthentication.class);
+        Library library = Mockito.mock(Library.class);
+        PromptUser promptUser = new PromptUser(view, user, library);
+        when(user.authenticate("999-1234", "password")).thenReturn(false);
+
+        promptUser.execute();
+
+        verify(view).print("Enter user ID : ");
+        verify(view).print("Enter Password: ");
+        verify(view, times(2)).input();
+        verify(view).print("The userID and password does not match. Try again.");
     }
 }
